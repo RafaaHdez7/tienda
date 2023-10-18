a90cb3f1c67fe62c7fb7351b2cb024eb5711135d
RafaaHdez7 <rafahdezgomez@gmail.com>
2023-10-18 23:17:08 +0200

    Update PedidoService.java

--- backend/backendTienda2/src/main/java/backend/rafhergom/tfg/service/PedidoService.java
+++ backend/backendTienda2/src/main/java/backend/rafhergom/tfg/service/PedidoService.java
@@ -5,8 +5,13 @@ import org.springframework.stereotype.Service;
 
 import backend.rafhergom.tfg.model.dtos.PedidoDTO;
 import backend.rafhergom.tfg.model.entity.Pedido;
+import backend.rafhergom.tfg.model.entity.Usuario;
 import backend.rafhergom.tfg.repository.PedidoRepository;
+import backend.rafhergom.tfg.repository.UsuarioRepository;
+
 import java.util.ArrayList;
+import java.time.LocalDateTime;
+
 
 
 import java.util.List;
@@ -14,12 +19,14 @@ import java.util.Optional;
 
 @Service
 public class PedidoService {
-
+	private final UsuarioRepository usuarioRepository;
     private final PedidoRepository pedidoRepository;
+    
 
     @Autowired
-    public PedidoService(PedidoRepository pedidoRepository) {
+    public PedidoService(PedidoRepository pedidoRepository, UsuarioRepository usuarioRepository) {
         this.pedidoRepository = pedidoRepository;
+        this.usuarioRepository = usuarioRepository;
     }
 
     public List<PedidoDTO> obtenerTodosLosPedidos() {
@@ -56,15 +63,54 @@ public class PedidoService {
     }
 
     public PedidoDTO crearPedido(PedidoDTO pedidoDTO) {
-        // Implementa la lógica para crear un pedido a partir del DTO.
-        // Puedes utilizar el repository y el convertidor de DTOs aquí.
-        return null;
+        // Crea un nuevo objeto Pedido a partir del DTO
+        Pedido nuevoPedido = new Pedido();
+        nuevoPedido.setEstadoPedido(pedidoDTO.getEstadoPedido());
+        nuevoPedido.setFechaHora(LocalDateTime.parse(pedidoDTO.getFechaHora())); // Asegúrate de manejar la conversión apropiadamente
+        // Aquí, también necesitarías obtener el usuario correspondiente y asignarlo al pedido
+        Usuario usuario = usuarioRepository.findById(pedidoDTO.getUsuarioId()).orElse(null);
+        nuevoPedido.setUsuario(usuario);
+
+        // Guarda el nuevo pedido en la base de datos utilizando el repository
+        nuevoPedido = pedidoRepository.save(nuevoPedido);
+
+        // Crea un DTO para el nuevo pedido y devuélvelo
+        PedidoDTO nuevoPedidoDTO = new PedidoDTO();
+        nuevoPedidoDTO.setId(nuevoPedido.getId());
+        nuevoPedidoDTO.setEstadoPedido(nuevoPedido.getEstadoPedido());
+        nuevoPedidoDTO.setFechaHora(nuevoPedido.getFechaHora().toString());
+        // También configura otros campos según sea necesario
+
+        return nuevoPedidoDTO;
     }
 
     public PedidoDTO actualizarPedido(Long id, PedidoDTO pedidoDTO) {
-        // Implementa la lógica para actualizar un pedido a partir del DTO.
-        // Puedes utilizar el repository y el convertidor de DTOs aquí.
-        return null;
+        // Verifica si el pedido con el ID proporcionado existe en la base de datos
+        Optional<Pedido> pedidoOptional = pedidoRepository.findById(id);
+        if (pedidoOptional.isPresent()) {
+            // Obtén el pedido existente
+            Pedido pedidoExistente = pedidoOptional.get();
+
+            // Actualiza los campos relevantes del pedido existente con los valores del DTO
+            pedidoExistente.setEstadoPedido(pedidoDTO.getEstadoPedido());
+            pedidoExistente.setFechaHora(LocalDateTime.parse(pedidoDTO.getFechaHora())); // Asegúrate de manejar la conversión apropiadamente
+            // También puedes necesitar actualizar otros campos
+
+            // Guarda la actualización en la base de datos
+            pedidoExistente = pedidoRepository.save(pedidoExistente);
+
+            // Crea un DTO para el pedido actualizado y devuélvelo
+            PedidoDTO pedidoActualizadoDTO = new PedidoDTO();
+            pedidoActualizadoDTO.setId(pedidoExistente.getId());
+            pedidoActualizadoDTO.setEstadoPedido(pedidoExistente.getEstadoPedido());
+            pedidoActualizadoDTO.setFechaHora(pedidoExistente.getFechaHora().toString());
+            // También configura otros campos según sea necesario
+
+            return pedidoActualizadoDTO;
+        } else {
+            // Si el pedido con el ID proporcionado no existe, puedes manejarlo apropiadamente
+            return null;
+        }
     }
 
     public void eliminarPedido(Long id) {
