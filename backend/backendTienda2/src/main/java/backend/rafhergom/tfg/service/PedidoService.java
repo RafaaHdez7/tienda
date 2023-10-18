286e7fc42666f1d6ddbeba2c83cb9a835c310e41
RafaaHdez7 <rafahdezgomez@gmail.com>
2023-10-18 23:08:14 +0200

    completo el obtenerTodosLosPedidos

--- backend/backendTienda2/src/main/java/backend/rafhergom/tfg/service/PedidoService.java
+++ backend/backendTienda2/src/main/java/backend/rafhergom/tfg/service/PedidoService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 import backend.rafhergom.tfg.model.dtos.PedidoDTO;
 import backend.rafhergom.tfg.model.entity.Pedido;
 import backend.rafhergom.tfg.repository.PedidoRepository;
+import java.util.ArrayList;
+
 
 import java.util.List;
 import java.util.Optional;
@@ -21,10 +23,23 @@ public class PedidoService {
     }
 
     public List<PedidoDTO> obtenerTodosLosPedidos() {
-        // Implementa la lógica para obtener todos los pedidos y convertirlos a DTOs.
-        // Puedes utilizar el repository y el convertidor de DTOs aquí.
-        return null;
+        List<Pedido> pedidos = pedidoRepository.findAll();
+        List<PedidoDTO> pedidoDTOs = new ArrayList<>();
+
+        for (Pedido pedido : pedidos) {
+            PedidoDTO pedidoDTO = new PedidoDTO();
+            // Copiar los atributos relevantes de Pedido a PedidoDTO
+            pedidoDTO.setEstadoPedido(pedido.getEstadoPedido());
+            pedidoDTO.setFechaHora(pedido.getFechaHora().toString());
+            pedidoDTO.setUsuarioId(pedido.getUsuario().getId());
+            pedidoDTO.setId(pedido.getId());
+            pedidoDTO.setUsuarioNombre(pedido.getUsuario().getNombre());
+            pedidoDTOs.add(pedidoDTO);
+        }
+
+        return pedidoDTOs;
     }
+
 
     public PedidoDTO obtenerPedidoPorId(Long id) {
         Optional<Pedido> pedidoOptional = pedidoRepository.findById(id);
